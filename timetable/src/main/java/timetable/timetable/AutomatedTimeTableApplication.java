ea6799f6ddaee947d4d86cb90d41b5e9b016e5b4
Aathil <aathilnishad@gmail.com>
2025-03-08 22:59:26 +0530

    Added the Springboot

---
+++ timetable/src/main/java/timetable/timetable/AutomatedTimeTableApplication.java
@@ -0,0 +1,13 @@
+package timetable.timetable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomatedTimeTableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomatedTimeTableApplication.class, args);
+	}
+
+}
---
+++ timetable/src/test/java/timetable/timetable/AutomatedTimeTableApplicationTests.java
@@ -0,0 +1,13 @@
+package timetable.timetable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomatedTimeTableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

2f91cec959170981fd936d76b3b74525eb0f8823
Aathil <aathilnishad@gmail.com>
2025-03-18 15:02:35 +0530

    added implementation

---
+++ Implementation/timetable-scheduler/src/main/java/com/university/scheduler/timetable_scheduler/TimetableSchduleApplication.java
@@ -0,0 +1,13 @@
+package com.university.scheduler.timetable_scheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimetableSchduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimetableSchduleApplication.class, args);
+	}
+
+}
---
+++ Implementation/timetable-scheduler/src/test/java/com/university/scheduler/timetable_scheduler/TimetableSchduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.university.scheduler.timetable_scheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimetableSchduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
